5778d4cab18f272196506e0f97cb447e840b475d
actualmira <ralzchrist@gmail.com>
2026-03-12 13:12:05 +0000

    feat: Add baseline DVWA and MariaDB deployment with pod-to-pod connection verified

---
+++ k8s/secrets.example.sh
@@ -0,0 +1,5 @@
+kubectl create secret generic mariadb-secret \
+  --from-literal=root-password=YOUR_ROOT_PASSWORD \
+  --from-literal=db-user=YOUR_DB_USER \
+  --from-literal=db-password=YOUR_DB_PASSWORD \
+  --namespace <the_namespace>
